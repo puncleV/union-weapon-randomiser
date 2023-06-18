9a48ff8c9513c4f8fdb87954abcf21fb635f899c
puncleV <punkrus18@gmail.com>
2023-06-18 13:30:46 +0300

    (add) ranged weapons randomiser

--- RandomiseWeaponsCommand.cpp
+++ RandomiseWeaponsCommand.cpp
@@ -48,11 +48,57 @@ namespace GOTHIC_ENGINE {
         return weapons_list;
     }
 
+    zCArray<int> getRangeWeaponsList() {
+        zCArray<int> weapons_list;
+        auto c_item = parser->GetIndex(oCItem::classDef->scriptClassName);
+        if (c_item == -1) {
+            return 0;
+        }
+
+        int itemsCreated = 0;
+        for (int i = 0; i < parser->symtab.GetNumInList(); i++) {
+            zCPar_Symbol* symbol = parser->symtab.table[i];
+
+            if (i == parser->instance_help) {
+                continue;
+            }
+
+            if (symbol->type != zPAR_TYPE_INSTANCE) {
+                continue;
+            }
+
+            //  Instance isn't global scope
+            if (symbol->name.HasWord(".")) {
+                continue;
+            }
+
+            auto baseClass = parser->GetBaseClass(symbol);
+
+            // Symbol isn't a item class
+            if (baseClass != c_item) {
+                continue;
+            }
+
+            oCItem* item = static_cast<oCItem*>(ogame->GetGameWorld()->CreateVob(zVOB_TYPE_ITEM, i));
+
+            if (item->mainflag != ITM_CAT_FF) {
+                continue;
+            }
+  
+            item->Release();
+
+            weapons_list.Insert(i);
+        }
+
+        return weapons_list;
+    }
     int RandomiseWeapons() {
-        auto weaponsList = getMeleeWeaponsList();
+        auto meleeWeaponsList = getMeleeWeaponsList();
+        auto rangedWeaponsList = getRangeWeaponsList();
+
         oCWorld* world = dynamic_cast<oCWorld*>(ogame->GetWorld());
         auto npcsCount = 0;
-
+        
         if (world) {
             auto list = world->voblist_npcs;
             for (size_t i = 0; i < list->GetNumInList(); ++i)
@@ -63,7 +109,7 @@ namespace GOTHIC_ENGINE {
                     continue;
 
                 for (;;) {
-                    int weaponId = weaponsList[randomizer.Random(0, weaponsList.GetNumInList())];
+                    int weaponId = meleeWeaponsList[randomizer.Random(0, meleeWeaponsList.GetNumInList())];
                     oCItem* item = static_cast<oCItem*>(ogame->GetGameWorld()->CreateVob(zVOB_TYPE_ITEM, weaponId));
 
                     if (npcCanWearWeapon(item, npc)) {
@@ -81,10 +127,34 @@ namespace GOTHIC_ENGINE {
                     }
                 }
 
+                auto equippedRangedItem = npc->GetEquippedRangedWeapon();
+                if (equippedRangedItem != nullptr || randomizer.Random(0, 100) >= 70) {
+                    for (;;) {
+                        int weaponId = rangedWeaponsList[randomizer.Random(0, rangedWeaponsList.GetNumInList())];
+                        oCItem* item = static_cast<oCItem*>(ogame->GetGameWorld()->CreateVob(zVOB_TYPE_ITEM, weaponId));
+
+                        if (npcCanWearWeapon(item, npc)) {
+                            npcsCount += 1;
+
+                            if (equippedRangedItem != nullptr) {
+                                npc->UnequipItem(equippedRangedItem);
+                                npc->RemoveFromInv(equippedRangedItem, 1);
+                            }
+
+                            npc->PutInInv(item);
+                            npc->EquipItem(item);
+
+                            item->Release();
+
+                            break;
+                        }
+                    }
+                }
             }
         }
 
-        weaponsList.DeleteList();
+        meleeWeaponsList.DeleteList();
+        rangedWeaponsList.DeleteList();
 
         return npcsCount;
     }
